a4cabd0742aea12ec041b3b1b353090503b7349e
Pranto Kumar <prantokumar.net@gmail.com>
2024-10-31 14:01:06 +0600

    added logic and player input

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
-{
+{   
+        int max, min, guess;
     
     // Start is called before the first frame update
     void Start()
-    {   int max = 1000;
-        int min = 1;
-
+    {   
+        StartGame();
+    }
+    void StartGame(){
+        max = 1000;
+        min = 1;
+        guess = 500;
         print("Develop By Pranto!");
         Debug.Log("Welcome to Number Wizard, yo!");//debug and print are same pretty much;
         Debug.Log("Pick a number in your head, and don't tell anyone!");
@@ -17,19 +22,26 @@ public class HelloWorld : MonoBehaviour
         Debug.Log($"The lowest number you can pick is : {min}");
         Debug.Log("Think you want to say : ");
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
     }
-
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.UpArrow) ){
-            Debug.Log("Up Arrow key was pressed.");
+            min = guess; // min  = 500;
+            NextGuess();
         }
         else if(Input.GetKeyDown(KeyCode.DownArrow) ){
-            Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            NextGuess();
         }
         else if(Input.GetKeyDown(KeyCode.Return)){
-            Debug.Log("Enter Key Pressed.");
+            Debug.Log("!!!!!!!!! YOU WIN !!!!!!!!!!");
+            StartGame();
         }
+    }
+    void NextGuess(){
+        guess = (max + min) / 2;
+        Debug.Log("Is the number is higher or lower then..." + guess);
     }
 }
